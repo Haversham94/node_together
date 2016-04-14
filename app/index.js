e0bc7757e1c37b951782cae96ab203196178497c
Haversham24 <rodolpheaguidissou@gmail.com>
2016-04-14 17:22:09 +0200

    add and configure view engine & add layouts

--- app/index.js
+++ app/index.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var app = express();
+var motivations = require('motivations');
+var pickone = require('pick-one');
+var exphdbs = require('express-handlebars');
+
+//declare engine
+app.engine('handlebars', exphdbs({
+    defaultLayout: "main" ,
+    layoutsDir: "./app/views/layouts"
+
+}));
+
+app.set('views', './app/views');
+app.set('view engine', 'handlebars');
 
 app.get("/", function(req, res){
 
-    res.send("Hey !! I am watching you !(^_^)");
+    //res.send("Hey !! I am watching you !(^_^)" + pickone(motivations));
+    var motivation = "coucou !!";
+    res.render('motivation');
 
 });
 
 app.get("/about", function(req, res){
 
-    res.send("Hey !! I am steal watching you !(*_^)");
 
 });
 
